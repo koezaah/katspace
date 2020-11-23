3355d2d06f60a797ccba7e3848ce4eac36796751
kat <>
2020-11-23 21:16:54 +0100

    clean scss monile view, color fix

--- js/app.js
+++ js/app.js
@@ -5,24 +5,6 @@ import './../css/style.scss';
 import artworkImages from './../img/artwork/*.jpg';
 import printImages from './../img/print/*.jpg';
 
-
-if (window.innerWidth < 800) {
-  window.addEventListener('scroll', function (e) {
-    const nav = document.getElementsByTagName('nav')[0];
-    const sections = [...document.getElementsByTagName('section')];
-
-    const asideHeight = document.getElementsByTagName('aside')[0].clientHeight;
-    if (window.scrollY >= asideHeight && window.innerWidth < 800) {
-      nav.style.position = 'fixed';
-      nav.style.top = 0;
-      sections.forEach((element) => (element.style.paddingTop = '80px'));
-    } else {
-      nav.style.position = 'unset';
-      sections.forEach((element) => (element.style.paddingTop = '0'));
-    }
-  });
-}
-
 const app = new Vue({
   el: '#app',
   data: {
